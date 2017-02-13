5957179e69709599a3479b21f39f59b220bafbbf
Antoine Lestrade <alestrade@alten.be>
2017-02-13 16:35:36 +0100

    [emacs] Misc improvments
    remove assignment to void function
    add init script (windows)

--- emacs/config/init-company.el
+++ emacs/config/init-company.el
@@ -6,8 +6,8 @@
   (with-eval-after-load 'company
     (diminish 'company-mode "CMP"))
     (when (maybe-require-package 'company-quickhelp)
-      (with-eval-after-load 'company-quickhelp
-        (define-key company-quickhelp-mode-map (kbd "M-h") nil))
+      ;(with-eval-after-load 'company-quickhelp
+      ;  (define-key company-quickhelp-mode-map (kbd "M-h") nil))
       (add-hook 'after-init-hook 'company-quickhelp-mode)))
 
 (setq company-tootltip-flip-when-above t)
