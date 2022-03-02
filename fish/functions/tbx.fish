41c7b9dc82ccb43690e2890f02dad68769e6e0ee
Antoine Lestrade <antoine.lestrade@gmail.com>
2022-03-02 08:54:13 +0100

    Update tmx conf + vm helpers

--- fish/functions/tbx.fish
+++ fish/functions/tbx.fish
@@ -9,11 +9,12 @@ function tbx
             m3-toolbox module stop "macq-m3-"$argv[2..-1]
         case mongo
             m3-toolbox mongo create
+            mongo -u antoine -p antoine --eval 'db.getSiblingDB("m3config").schedulerConfigurations.update({id: "iCar_recovery"}, { $set: { enabled: false } })'
         case kafka
             m3-toolbox kafka create
         case keycloak
-            m3-toolbox keycloak clean
-            m3-toolbox keycloak initialize
+            m3-toolbox keycloak clean --adm-user macq --adm-password itsobvious
+            m3-toolbox keycloak initialize --adm-user macq --adm-password itsobvious
         case setup
             m3-toolbox mongo create
             m3-toolbox kafka create
