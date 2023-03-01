b5d5238bfcf164cde566e439c0bb7e83770f1733
Antoine Lestrade <antoine.lestrade@gmail.com>
2023-03-01 20:51:32 +0100

    [pwsh] Add aliases

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,9 @@
 Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+
+Set-PSReadLineKeyHandler -Chord Ctrl+d -Function DeleteCharOrExit
+
+function exa-full {exa  -alhb --git --icons $args}
+New-Alias ll exa-full
+
 oh-my-posh init pwsh --config $PROFILE/../theme.omp.json | Invoke-Expression
 $env:POSH_GIT_ENABLED = $true
