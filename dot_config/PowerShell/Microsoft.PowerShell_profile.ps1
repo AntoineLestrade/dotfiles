43f826243f8e1bb1c3b61b7c1de522227ecd52ef
Antoine Lestrade <antoine.lestrade@gmail.com>
2023-03-05 11:37:29 +0100

    [pwsh] Add config

---
+++ dot_config/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,10 @@
+Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+
+Set-PSReadLineKeyHandler -Chord Ctrl+d -Function DeleteCharOrExit
+
+function exa-full {exa  -alhb --git --icons $args}
+New-Alias ll exa-full
+
+$THEME_PATH = Join-Path -Path $script:MyInvocation.MyCommand.Path -ChildPath '\..\theme.omp.json'
+oh-my-posh init pwsh --config $THEME_PATH | Invoke-Expression
+$env:POSH_GIT_ENABLED = $true
