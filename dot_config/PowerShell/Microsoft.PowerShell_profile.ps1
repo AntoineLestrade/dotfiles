4d66b15f0c5a2765b7fc87cfca75815ea9d8b474
Antoine Lestrade <antoine.lestrade@gmail.com>
2024-07-24 07:44:59 +0200

    Small update

--- dot_config/PowerShell/Microsoft.PowerShell_profile.ps1
+++ dot_config/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -5,6 +5,7 @@ Set-PSReadLineKeyHandler -Chord Ctrl+d -Function DeleteCharOrExit
 function exa-full {exa  -alhb --git --icons $args}
 New-Alias ll exa-full
 
+#Invoke-Expression (&starship init powershell)
 $THEME_PATH = Join-Path -Path $script:MyInvocation.MyCommand.Path -ChildPath '\..\theme.omp.json'
 oh-my-posh init pwsh --config $THEME_PATH | Invoke-Expression
 $env:POSH_GIT_ENABLED = $true
